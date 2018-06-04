d51a97dc28d8319c855f8d2cadcf270566e61d39
Petro Narytnyk <pnarytnyk@softjourn.com>
2018-06-04 13:53:19 +0300

    another fix

--- initial.py
+++ initial.py
@@ -1,9 +1,11 @@
+from apscheduler.schedulers.blocking import BlockingScheduler
 import os
 import slackclient
 from slackclien import SlackClient as SC
 
+sched = BlockingScheduler()
 
-os.environ['slc']=
+os.environ['slc']='xoxp-4120244611-147108286259-374083396053-3a58df311fc7d1d35e396131471a1967'
 token = os.environ['slc']
 sc=SC(token)
 
@@ -17,13 +19,16 @@ def post_to_chat(chat, mess):
 	)
 
 def post_user(user, mess):
-	
+	sc.api_call(
+	  "chat.postMessage",
+	  channel="@{}".format(user),
+	  text="{}".format(mess),
+	  username='just a prank',
+	  icon = ':rolling_on_the_floor_laughing:'
+	)
 
+@sched.scheduled_job('interval', minutes=5)
+def post_me():
+	post_user('pnarytnyk', 'hi bruh, im working')
 
-sc.api_call(
-  "chat.postMessage",
-  channel="@pnarytnyk",
-  text="Hello from Python! :tada:",
-  username='just a prank',
-  icon = ':rolling_on_the_floor_laughing:'
-)
+sched.start()
