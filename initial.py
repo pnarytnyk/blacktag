9e130368c5ca61aaaa514d7ceea06fc9773652bf
Petro Narytnyk <pnarytnyk@softjourn.com>
2018-06-04 15:00:54 +0300

    fif

--- initial.py
+++ initial.py
@@ -1,11 +1,10 @@
-from apscheduler.schedulers.blocking import BlockingScheduler
+# from apscheduler.schedulers.blocking import BlockingScheduler
 import os
 import slackclient
 from slackclient import SlackClient as SC
 
-sched = BlockingScheduler()
+# sched = BlockingScheduler()
 
-os.environ['slc']='xoxp-4120244611-147108286259-374083396053-3a58df311fc7d1d35e396131471a1967'
 token = os.environ['slc']
 sc=SC(token)
 
@@ -27,8 +26,8 @@ def post_user(user, mess):
 	  icon = ':rolling_on_the_floor_laughing:'
 	)
 
-@sched.scheduled_job('interval', minutes=5)
+# @sched.scheduled_job('interval', minutes=5)
 def post_me():
 	post_user('pnarytnyk', 'hi bruh, im working')
 
-sched.start()
+# sched.start()
