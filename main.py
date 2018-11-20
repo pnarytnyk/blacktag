64ea6c328d891b3d7e720d38b5688e13f9c3dbe6
unknown <pnarytnyk@softjourn.com>
2018-11-20 10:32:10 +0200

    add support for commands

--- main.py
+++ main.py
@@ -38,7 +38,7 @@ def hello1(ass):
         # })
         # ds.put(entity)
         # return str(dir(request))
-        return str(request.args) + '\n\n'+str(request.view_args) + '\n\n'+ '\n\n' + str(request.get_data()) + "\n\n" +  "\n\n" + str(request.data())
+        return str(request.args) + '\n\n'+str(request.view_args) + '\n\n'+ '\n\n' + str(request.get_data()) + "\n\n" + str(request.parameter_storage_class)+ "\n\n" + str(request.data)
 
     elif request.method == 'DELETE':
         # with open('sraka.txt','w+') as f:
